e7baa80d51b44feecab37e9801f0889ee5034874
Elka Pratika Ilman <elkapilman@gmail.com>
2026-07-29 23:24:45 +0700

    fix: create refund failed

--- src/hooks/useRefund.ts
+++ src/hooks/useRefund.ts
@@ -1,11 +1,18 @@
 import { useMutation } from '@tanstack/react-query';
 
 import { createRefund } from '@/api';
+import { DEFAULT_ERROR_MESSAGE } from '@/constants/common';
 import type { RefundPayloadType } from '@/types';
 
 export default function useRefund() {
   const mutation = useMutation({
-    mutationFn: (payload: RefundPayloadType) => createRefund(payload),
+    mutationFn: async (payload: RefundPayloadType) => {
+      const response = await createRefund(payload);
+      if (!response.success) {
+        throw new Error(response.message ?? DEFAULT_ERROR_MESSAGE);
+      }
+      return response;
+    },
   });
 
   return {
